1379447ed53914c46c6ddb156753bd779712561a
David Oghi <davsong16@gmail.com>
2022-08-05 02:33:17 +0100

    added seed for 50 students

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -1,8 +1,12 @@
 <?php
 namespace Database\Seeders;
 
+use Faker\Factory;
+use App\Models\User;
+// use Illuminate\Database\Seeder;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Hash;
 use Spatie\Permission\Models\Permission;
 
 class RolesAndPermissionsSeeder extends Seeder
@@ -14,11 +18,47 @@ class RolesAndPermissionsSeeder extends Seeder
      */
     public function run()
     {
-        // Reset cached roles and permissions
-        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+        $count = range(1, 51);
+        
+        $faker = Factory::create();
 
-        // create permissions
-        $role = Role::create(['name' => 'Admin']);
-        $role = Role::create(['name' => 'Student']);
+        foreach($count as $count){
+           
+            $user = User::create([
+                'name'          => $faker->name(),
+                'middlename'          => $faker->name(),
+                'surname'          => $faker->word,
+                'email'         => $faker->email(),
+                'password'      => bcrypt('12345678'),
+                'created_at'    => date("Y-m-d H:i:s")
+            ]);
+
+            $user->student()->create([
+                'department_id' => 6,
+                'faculty_id' => 3,
+                'locked' => 0,
+                "program" => 'BSC',
+                "level" => 1,
+                "semester" => 1,
+                "year_of_admission" => '2021/2022',
+                "gender" => $faker->randomElement(["male", "female", "other"]),
+            ]);
+
+
+            $user->update([
+                'matric' => app('App\Http\Controllers\Controller')->generateMatric($user),
+                'password' => Hash::make(strtoupper($user->surname)),
+            ]);
+
+            $user->assignRole('Student');
+        }
+       
+       
+        // Reset cached roles and permissions
+        // app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // // create permissions
+        // $role = Role::create(['name' => 'Admin']);
+        // $role = Role::create(['name' => 'Student']);
     }
 }
